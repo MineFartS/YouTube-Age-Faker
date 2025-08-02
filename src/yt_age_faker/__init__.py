232dea3840f27c82c44063df6c32884fad470eb1
MineFartS <143120468+MineFartS@users.noreply.github.com>
2025-08-02 13:32:34 -0400

    Update __init__.py

--- src/yt_age_faker/__init__.py
+++ src/yt_age_faker/__init__.py
@@ -2,3 +2,13 @@ from philh_myftp_biz import *
 import sys
 
 m = sys.argv[1].lower()
+
+print(m)
+
+if m == 'setup':
+    ''
+    
+elif m == 'run':
+    
+    with web.browser() as b:
+        b.open('https://YouTube.com')
