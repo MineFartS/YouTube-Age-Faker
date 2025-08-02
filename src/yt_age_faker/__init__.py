9d87ae295f5d2684649f43cee75449d3f8889032
MineFartS <143120468+MineFartS@users.noreply.github.com>
2025-08-02 13:27:44 -0400

    Update __init__.py

--- src/yt_age_faker/__init__.py
+++ src/yt_age_faker/__init__.py
@@ -1,12 +1,4 @@
-import sys, subprocess
+from philh_myftp_biz import *
+import sys
 
-method = sys.argv[1].lower()
-
-def installPKG(pkg):
-    subprocess.run([sys.executable, '-m', 'pip', 'install', pkg])
-
-try:
-    from philh_myftp_biz import *
-except:
-    installPKG("git+https://github.com/MineFartS/Server-Package")
-    from philh_myftp_biz import *
+m = sys.argv[1].lower()
